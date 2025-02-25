126afaab17b96bb869c325677c30a22801431878
“lancerstadium” <lancerstadium@163.com>
2025-02-25 15:12:00 +0800

    train SR

--- wllab/network/lut.py
+++ wllab/network/lut.py
@@ -3766,6 +3766,7 @@ class LogicLUTNet(nn.Module):
         super(LogicLUTNet, self).__init__()
         self.kernel_size = kernel_size
         self.upscale = upscale
+        self.scale = nn.Parameter(torch.ones([1, self.upscale * self.upscale, 1, 1]) * 0.1)
         self.dw_msb = DepthwiseLUT(kernel_size=kernel_size, out_channels=upscale ** 2)
         self.dw_lsb = DepthwiseLUT(kernel_size=kernel_size, out_channels=upscale ** 2)
         self.pw_msb = PointwiseLUT(upscale=upscale, n_feature=n_feature)
@@ -3827,8 +3828,9 @@ class LogicLUTNet(nn.Module):
         lsb1 = self.dw_lsb(lsb).clamp(0, 3)
         msb2 = self.pw_msb(msb1).clamp(-32, 31)
         lsb2 = self.pw_lsb(lsb1).clamp(0, 3)
-        res2 = ((msb2 * 2 + floor_ste(lsb2 / 2)).clamp(-128, 127) + floor_ste(x / 2).clamp(-128, 127)).clamp(-128, 127)
-        res = nn.PixelShuffle(self.upscale)(res2)
+        res1 = floor_ste((msb2 * 4 + lsb2).clamp(-128, 127) * self.scale).clamp(-128, 127)
+        res2 = floor_ste(x * (1 - self.scale)).clamp(-128, 127)
+        res = nn.PixelShuffle(self.upscale)((res1 + res2).clamp(-128, 127))
         res = self.enhance(res).clamp(-128, 127)
         # Batch to channel: [N * C, 1, H, W] -> [N, C, H, W]
         res = res.view(-1, C, res.size(2), res.size(3))
