5b617ea8ac9d282835f17a532675ce0259c1ebf3
“lancerstadium” <lancerstadium@163.com>
2025-02-25 11:10:36 +0800

    train SR

--- wllab/network/lut.py
+++ wllab/network/lut.py
@@ -3793,10 +3793,10 @@ class LogicLUTNet(nn.Module):
         C = x.size(1)
         x = x.view(-1, 1, x.size(2), x.size(3))
         msb, lsb = self.seg(x)
-        msb1 = self.dw_msb(msb).clamp(-32, 31).floor()
-        lsb1 = self.dw_lsb(lsb).clamp(0, 3).floor()
-        msb2 = self.pw_msb(msb1).clamp(-32, 31).floor()
-        lsb2 = self.pw_lsb(lsb1).clamp(0, 3).floor()
+        msb1 = self.dw_msb(msb).clamp(-32, 31).floor() + msb
+        lsb1 = self.dw_lsb(lsb).clamp(0, 3).floor() + lsb
+        msb2 = self.pw_msb(msb1).clamp(-32, 31).floor() + msb1
+        lsb2 = self.pw_lsb(lsb1).clamp(0, 3).floor() + lsb1
         res = (msb2 * 4 + lsb2).clamp(-128, 127).floor()
         res = nn.PixelShuffle(self.upscale)(res)
         # Batch to channel: [N * C, 1, H, W] -> [N, C, H, W]
