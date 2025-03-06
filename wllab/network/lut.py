e3f5751a311059b0f9b5a5f0ff8e46224a7bcb58
“lancerstadium” <lancerstadium@163.com>
2025-03-06 18:10:44 +0800

    train SR

--- wllab/network/lut.py
+++ wllab/network/lut.py
@@ -4714,7 +4714,7 @@ class VarLUTNet(nn.Module):
         self.Round = round_fn
         self.upscale = upscale
         self.shared_pw = VarPointwise(upscale=upscale,out_ch=3 * upscale * upscale,n_feature=n_feature,round_fn=round_fn)
-        self.head = VarLUTResBlock(upscale=1, in_ch = 3 * upscale * upscale,out_ch= 3 * upscale * upscale,n_feature=n_feature)
+        self.head = VarLUTResBlock(upscale=1, in_ch = 3 ,out_ch= 3 * upscale * upscale,n_feature=n_feature)
         self.blk1 = VarLUTResBlock(upscale=4, out_ch= 3 * upscale * upscale,n_feature=n_feature,pw2=self.shared_pw)
         self.blk2 = VarLUTResBlock(upscale=4, out_ch= 3 * upscale * upscale,n_feature=n_feature,pw2=self.shared_pw)
         self.blk3 = VarLUTResBlock(upscale=4, out_ch= 3 * upscale * upscale,n_feature=n_feature,pw2=self.shared_pw)
