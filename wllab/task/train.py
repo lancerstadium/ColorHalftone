81d4bebc09554d9a638f62a96efa019b9b899496
“lancerstadium” <lancerstadium@163.com>
2025-03-09 18:01:17 +0800

    train SR

--- wllab/task/train.py
+++ wllab/task/train.py
@@ -374,7 +374,7 @@ def train_cf(model, dataloader, num_epochs=50,save_path=None):
         
                 pbar.set_postfix({
                     "loss": f"{running_loss / (pbar.n + 1):.5f}",
-                    "cls": f"{predicted.sum().item() / labels.sum().item():.5f}",
+                    "cls": f"{predicted.sum().item()} / {labels.sum().item():.5f}",
                     "acc": f"{correct / total:.5f}"
                 })
                 pbar.update(1)
